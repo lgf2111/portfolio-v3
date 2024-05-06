00259b23d377248a73e98da650efb13fda247e5c
Jeff Lee <lgf2111@gmail.com>
2024-05-07 04:28:58 +0800

    fix

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,4 +1,5 @@
 // Input component extends from shadcnui - https://ui.shadcn.com/docs/components/input
+"use client";
 
 import * as React from "react";
 import { cn } from "@/utils/cn";
